f10ac261e9e0f95c9e46f94ab4e98c24b07dbb09
harvey magana <heyharv2012@gmail.com>
2021-01-23 14:41:17 -0800

    Trying to get my projects file to send a response from the request and to try to post with the available post method. Not working so far.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -13,15 +13,31 @@ const router = express.Router();
     getProjectActions,
  */
 
- router.get('/projects', (req, res) => {
-    Projects.get(req.query)
-    .then(projects => {
-      res.status(200).json(projects)
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json({
-        message: "Error retreiving projects."
+ router.get('/:id', (req, res) => { // this request is not working...
+    const { id } = req.params;
+
+    Projects.get(id)
+      .then((item) => {
+        if (item) {
+          res.json(item);
+        } else {
+          res.status(404).json({ message: "Could not find item with given id." });
+        }
       })
-    })
+      .catch((err) => {
+        res.status(500).json({ message: "Failed to get item" });
+      });
+ })
+
+ router.post('/projects', (req, res) => {
+     Projects.insert(req.body)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: "Error adding project"
+            })
+        })
  })
