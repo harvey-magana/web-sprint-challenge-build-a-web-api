c8bd94908b07f5793e062c835cf3e64620123636
harvey magana <heyharv2012@gmail.com>
2021-01-23 13:41:40 -0800

    Second attempt at removing the node_modules file to fix the issue with sql.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,27 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const Projects = require('./projects-model.js');
+
+const router = express.Router();
+
+/**
+ *   get,
+    insert,
+    update,
+    remove,
+    getProjectActions,
+ */
+
+ router.get('/projects', (req, res) => {
+    Projects.get(req.query)
+    .then(projects => {
+      res.status(200).json(projects)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retreiving projects."
+      })
+    })
+ })
--- api/server.js
+++ api/server.js
@@ -4,7 +4,11 @@ const server = express();
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
+const ProjectsRouter = require('./projects/projects-router.js');
+
 server.use(express.json());
+
+server.use('/api/projects/', ProjectsRouter);
 
 server.get('/', (req, res) => {
     res.send(`<h2>My Express Sprint Project</h2>`);
