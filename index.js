a1b3f6f21840b7cae0fa1823930c6b2a02207d6a
harvey magana <heyharv2012@gmail.com>
2021-01-23 12:53:53 -0800

    Added the code for index.js to use and listen to port 5000.

--- index.js
+++ index.js
@@ -32,3 +32,10 @@ Test
 // const { middlewarefunctionname } = require("../users/middlewarefile.js");
 
 */
+const server = require('./api/server.js');
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+    console.log(`\n*** server listening on port ${PORT} ***\n`);
+});
